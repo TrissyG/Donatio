6207e4eba7ffa75f47f6b83ca6aaf2c89464ab8e
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-07-13 15:56:43 +1200

    Modified function to write to cloud storage

--- donatio/src/firebase/setup.ts
+++ donatio/src/firebase/setup.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDL4N9Fmo3Seoh8OuFbDJx_ZHZSIj9Fd2A",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
