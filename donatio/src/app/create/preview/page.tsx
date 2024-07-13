8d4a9c07df710ee5f4a71fa4e1de4b914553dd7b
Jason <jasonabc0626@gmail.com>
2024-07-13 17:46:30 +1200

    connect challenge to back

--- donatio/src/app/create/preview/page.tsx
+++ donatio/src/app/create/preview/page.tsx
@@ -1,8 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { completeChallenge } from "@/gateway/Challenges/putChallenges";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [loading, setLoading] = useState(false);
+
+  const onPost = async () => {
+    try {
+      setLoading(true);
+      await completeChallenge("1");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-center mt-8">
@@ -21,7 +35,10 @@ export default function Page() {
           className="rounded-lg shadow-md border-2 border-donatio-green border-opacity-50"
           rows={4}
         ></textarea>
-        <Button className="align-right self-end px-[50px] rounded-full bg-donatio-green text-[16px] font-bold">
+        <Button
+          className="align-right self-end px-[50px] rounded-full bg-donatio-green text-[16px] font-bold"
+          onClick={() => onPost}
+        >
           Post
         </Button>
       </form>
