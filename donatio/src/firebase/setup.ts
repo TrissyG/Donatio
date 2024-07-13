62dbab9f5785036ecded8cdc0ddbf73843db055c
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-07-13 20:03:27 +1200

    Migrated to new database

--- donatio/src/firebase/setup.ts
+++ donatio/src/firebase/setup.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGQ-kLsDZz3kFIm40wWfPsfxKfe5IrGhA",
-  authDomain: "donatio-deba9.firebaseapp.com",
-  projectId: "donatio-deba9",
-  storageBucket: "donatio-deba9.appspot.com",
-  messagingSenderId: "219121714489",
-  appId: "1:219121714489:web:eb7f88668128b7cb2c14e8",
+  apiKey: "AIzaSyDzjGWLP5AVhh9IuKJVl7U-uqcVpmZlVpM",
+  authDomain: "donatio-6b3f9.firebaseapp.com",
+  projectId: "donatio-6b3f9",
+  storageBucket: "donatio-6b3f9.appspot.com",
+  messagingSenderId: "129406158558",
+  appId: "1:129406158558:web:ebc7fce3d85cbf4d44a44f",
 };
 
 const app = initializeApp(firebaseConfig);
