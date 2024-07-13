68cc5fcfb881e46c3341385b5df00eafe1b27e8d
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-07-13 15:25:08 +1200

    Added posts function

--- donatio/src/types/types.ts
+++ donatio/src/types/types.ts
@@ -1,6 +1,17 @@
 export interface User {
-  causes: string;
+  causes: string[];
   donut_balance: number;
   donut_earned: number;
   donut_given: number;
+}
+
+export interface Post {
+  causes: string;
+  description: string;
+  image: string;
+  likes: number;
+  reposts: number;
+  time: string;
+  user_image: string;
+  username: string;
 }
