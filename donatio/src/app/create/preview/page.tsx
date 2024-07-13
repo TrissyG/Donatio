3aef83ae4b2bbcccea2e5d975861b473ebe4957c
boxy8 <zou.emily@yahoo.co.nz>
2024-07-14 01:31:32 +1200

    feat: pass image to preview and fix generation

--- donatio/src/app/create/preview/page.tsx
+++ donatio/src/app/create/preview/page.tsx
@@ -1,25 +1,47 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { completeChallenge } from "@/gateway/Challenges/putChallenges";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
+import { createPost } from "@/gateway/Posts/postPosts";
+import { Post } from "@/types/types";
+
 export default function Page() {
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const imageUrl = searchParams.get("imageUrl");
+  const [loading, setLoading] = useState(false);
+  const [newPost, setNewPost] = useState<Omit<Post, "image">>({
+    causes: "",
+    description: "",
+    likes: 0,
+    reposts: 0,
+    time: new Date().toISOString(),
+    user_image: "",
+    username: "",
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setNewPost({ ...newPost, [name]: value });
+  };
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
+    if (!imageUrl) {
+      console.error("No image URL found");
+      return;
+    }
     try {
       setLoading(true);
-      // await completeChallenge("1");
-      setTimeout(() => {
-        setLoading(false);
-      }, 1000);
+      await createPost(newPost, decodeURIComponent(imageUrl));
+      setLoading(false);
 
-      // router.push("http://localhost:3000/");
+      router.push("http://localhost:3000/");
     } catch (error) {
       console.log(error);
     }
@@ -28,22 +50,32 @@ export default function Page() {
   return (
     <div>
       <div className="flex justify-center mt-8">
-        <Image
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/donatio-deba9.appspot.com/o/posts%2F3vlh9Qthjo2epdtWdv9A?alt=media&token=b2b4efd3-abea-451a-a96e-18431807be94"
-          }
-          alt="image1"
-          width={350}
-          height={450}
-          className="rounded-2xl shadow-lg border-2 border-donatio-green"
-        />
+        {imageUrl && (
+          <Image
+            src={imageUrl}
+            alt="image1"
+            width={350}
+            height={450}
+            className="rounded-2xl shadow-lg border-2 border-donatio-green"
+          />
+        )}
       </div>
 
       <form className="flex flex-col gap-4 mx-8 mt-6">
-        <label className="text-lg font-semibold">Write a caption</label>
+        <label className="text-lg font-semibold">Enter a cause</label>
+        <input
+          type="text"
+          name="causes"
+          placeholder="Worthy cause"
+          onChange={handleInputChange}
+        />
+        <label className="text-lg font-semibold">Write a description</label>
         <textarea
+          name="description"
           className="rounded-lg shadow-md border-2 border-donatio-green border-opacity-50"
           rows={4}
+          placeholder="Why should people care about this cause?"
+          onChange={handleInputChange}
         ></textarea>
         <Button
           className="align-right self-end w-[150px] rounded-full bg-donatio-green text-[16px] font-bold"
