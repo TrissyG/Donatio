4cf4979fc8bacf32197d58feb7d4bb796dfbc8ee
Michelle <100409601+mlie788@users.noreply.github.com>
2024-07-14 12:08:23 +1200

    small chanegs

--- donatio/src/app/_components/posts/Card.tsx
+++ donatio/src/app/_components/posts/Card.tsx
@@ -28,13 +28,15 @@ const Card = ({ post }: CardProps) => {
   };
 
   return (
-    <div className="relative mx-4 flex items-center justify-center">
+    <div className="relative my-4 mx-6 flex items-center justify-center">
       <img
         src={post.image}
         alt={post.description}
-        className="rounded-lg mx-2 my-4 w-full h-full"
+        width={350}
+        height={350}
+        className="rounded-lg w-full h-full"
       />
-      <div className="absolute top-8 right-4 p-2 bg-black bg-opacity-50 rounded-lg cursor-pointer">
+      <div className="absolute top-4 right-4 p-2 bg-black bg-opacity-50 rounded-lg cursor-pointer">
         <div className="flex items-center justify-center gap-2">
           <HeartHandshake
             className={`${isLiked ? "text-red-500" : "text-white"}`}
@@ -43,11 +45,13 @@ const Card = ({ post }: CardProps) => {
           <p className="text-white cursor-default">{truncatedLikes} {truncatedLikes === 1 ? 'like' : 'likes'}</p>
         </div>
       </div>
-      <div className="absolute top-8 left-4 p-2 px-4 bg-black bg-opacity-50 text-white rounded-lg">
-        <p>{post.causes}</p>
-      </div>
+      {post.causes && (
+        <div className="absolute top-4 left-4 p-2 px-4 max-w-[200px] bg-black bg-opacity-50 text-white rounded-lg">
+          <p>{post.causes}</p>
+        </div>
+      )}
       <div
-        className={`absolute bottom-8 py-4 px-4 w-11/12 bg-black bg-opacity-50 text-white ${
+        className={`absolute bottom-4 py-4 px-4 w-11/12 bg-black bg-opacity-50 text-white ${
           isExpanded ? "rounded-lg bg-opacity-90" : "rounded-lg"
         }`}
       >
